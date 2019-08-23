35cda24d29b513e7e096e5209bf955baee4c93df
Ivan Sambrana <isambrana@invico.gov.ar>
2019-08-23 13:01:45 -0300

    classname

--- App.tsx
+++ App.tsx
@@ -1,29 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+// import { Container, Text } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+import * as FileSystem from 'expo-file-system';
+
+
 
 export default class App extends React.Component {
-   state;
-  constructor(props){
+  state
+  constructor(props) {
     super(props);
- 
-    this.state = 
-    { 
+    this.state = {
+      isReady: false,
+      file:'nada',
+      console:"c"
+    };
+  }
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+    // this.file();
+
+  }
+
+  file = async ()=>{
+    
+    var f = await FileSystem.readDirectoryAsync("file://sdcard/")
+    alert(f);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
     }
- }
 
+    return (
+      <Container style={{marginTop:Constants.statusBarHeight}}>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Prueba 23</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Button  onPress={()=>this.file()}>Probar</Button>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 
-
-
-  
- render() {
-  return (
-    <View>
-      Anda
-    </View>
-  );
+  // #20d6c7
 }
-
-
-
-};
