06c348a8f6d4069fb27746d3324f5b978c73f98c
Ivan Sambrana <isambrana@invico.gov.ar>
2019-08-23 11:47:36 -0300

    firt commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+   state;
+  constructor(props){
+    super(props);
+ 
+    this.state = 
+    { 
+
+    }
+ }
+
+
+
+
+  
+ render() {
+  return (
+    <View>
+      Anda
+    </View>
+  );
+}
+
+
+
+};
